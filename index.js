d8876c830e1bd478a29c1d15cfff19cbd7f60036
Jorge Y. C. Rodriguez <self@jorgerodriguez.dk>
2026-05-18 15:22:20 +0200

    feat: general build

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const { Octokit } = require('@octokit/action');
+import * as core from '@actions/core';
+import Octokit from '@octokit/action';
 
 async function run() {
   try {
